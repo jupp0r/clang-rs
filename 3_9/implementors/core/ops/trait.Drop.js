689c7a985d82fdad7b4cb5847c7cbfe75dd90b0a
Kyle Mayes <kyle@mayeses.com>
2016-07-22 03:34:48 -0400

    "Update documentation"

---
+++ 3_9/implementors/core/ops/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["clang"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='clang/completion/struct.CompletionResults.html' title='clang::completion::CompletionResults'>CompletionResults</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='clang/struct.Clang.html' title='clang::Clang'>Clang</a>","impl&lt;'c&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='clang/struct.Index.html' title='clang::Index'>Index</a>&lt;'c&gt;","impl&lt;'i&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='clang/struct.TranslationUnit.html' title='clang::TranslationUnit'>TranslationUnit</a>&lt;'i&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
